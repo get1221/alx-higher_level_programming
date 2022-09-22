e9d7314cd55580be58fb8dae8f2117f075f1de21
get1221 <get1221smart@gmail.com>
2022-09-22 10:30:02 -0700

    7 edges

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Get'
 word_first_3 = word[0:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("First 3 letters: {}" .format(word_first_3))
-print("last 2 letters: {}" .format(word_last_2))
+print("First 3 letters: {}".format(word_first_3))
+print("last 2 letters: {}".format(word_last_2))
 print("middle word: {}".format(middle(middle_word))
