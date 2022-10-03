a25104ec6531057cef957d42ca1a4e4cdb2a095f
get1221 <get1221smart@gmail.com>
2022-10-03 05:11:51 -0700

    calculator function

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ My add function
+    Args:
+    a: first numb: sec num
+    Returns:
+    the return val a and b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """ my sub funct
+    Args:
+    a: first num
+    b: sec num
+    Returns:
+    the return value a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """ my mul function
+    Args:
+    a: first number
+    b: sec number
+    Returns:
+    the return value a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """ my div function
+    Args:
+    a: first num
+    b: sec number
+    Returns:
+    the return value a / b
+    """
+    return int(a / b)
