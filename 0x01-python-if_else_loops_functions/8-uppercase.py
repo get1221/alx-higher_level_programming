afd636aac99b7727ccf40c4517192cb8f99b638b
get1221 <get1221smart@gmail.com>
2022-10-03 01:27:56 -0700

    print alphabet

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for i in str:
         i = ord(i)
-        if i >=97 and i <= 122:
+        if i >= 97 and i <= 122:
             i -= 32
             i = chr(i)
-            print ("{}".format(i), end='')
+            print("{}".format(i), end='')
             print("")
