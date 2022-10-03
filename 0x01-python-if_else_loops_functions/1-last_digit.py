3ba7ad750674f0712e2fe5452ed835852532e14d
get1221 <get1221smart@gmail.com>
2022-10-03 00:10:23 -0700

    last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,5 @@ else:
         print("is {} and is greater than 5".format(num))
     elif num == 0:
         print("is {} and is 0".format(num))
-   elif num < 6 and num != 0:
+   elif num < 6 and num != 0
        print("is {} and is less than 6 and not 0".format(num))
